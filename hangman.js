f3358e511464df706812d02a6ac66eb2987cb2e5
marcusryden92 <marcus.ryden1992@gmail.com>
2023-11-09 13:37:49 +0100

    Tried to change prompts to functions.

--- hangman.js
+++ hangman.js
@@ -1,5 +1,4 @@
-alert("HANGMAN");
-
+//gameText("HANGMAN");
 
 //We ask the user to input a word, and keep asking until
 //we're sure it's a word.
@@ -7,16 +6,15 @@ alert("HANGMAN");
 let userInputWord;
 
 while (true) {
-
-  userInputWord = prompt("Please enter a word!");
+  
+  userInputWord = playerPrompt("Please enter a word!");
 
   if (/^[a-zA-Z]+$/.test(userInputWord)) {
     break;
   } else {
-    alert("Only letters please!");
+    gameText("Only letters please!");
   }
 }
-
 
 let wordArray = [];
 let wordLength = userInputWord.length;
@@ -37,12 +35,12 @@ for (let i = 0; i < wordLength; i++) {
 }
 
 let displayString = displayArray.join(" ");
-alert(displayString);
+arrayText(displayString);
 
 //How many guesses are allowed?
 
 let guessesAllowed = 7;
-alert(`You have ${guessesAllowed} guesses!`);
+gameText(`You have ${guessesAllowed} guesses!`);
 
 let failedGuesses = 0;
 let correctGuesses = 0;
@@ -51,6 +49,7 @@ let correctGuesses = 0;
 //guesses is lower than the number of allowed guesses:
 
 while (failedGuesses < guessesAllowed) {
+
   //Prompting the player to guess a letter:
 
   let guessedLetter;
@@ -59,7 +58,7 @@ while (failedGuesses < guessesAllowed) {
   //otherwise we can keep guessing the same (correct) letter and still win.
 
   while (true) {
-    guessedLetter = prompt("Please guess a letter!");
+    guessedLetter = playerPrompt("Please guess a letter!");
 
     let originalLetter = true;
 
@@ -75,7 +74,7 @@ while (failedGuesses < guessesAllowed) {
     if (originalLetter) {
       break;
     } else {
-      alert("You have already guessed that letter!");
+      gameText("You have already guessed that letter!");
     }
   }
 
@@ -86,6 +85,7 @@ while (failedGuesses < guessesAllowed) {
   //Now we check if the letter corresponds to any letter in the word array:
 
   for (let i = 0; i < wordLength; i++) {
+
     //If the letters correspond, we add the letter - in caps - to the displayArray:
 
     if (compareLetters(guessedLetter, wordArray[i]) === true) {
@@ -111,7 +111,7 @@ while (failedGuesses < guessesAllowed) {
 
   if (correctGuesses === wordLength) {
     displayString = displayArray.join(" ");
-    alert(`${displayString} - You WIN!`);
+    gameText(`${displayString} - You WIN!`);
 
     break;
   } else if (failedGuesses === guessesAllowed) {
@@ -120,25 +120,71 @@ while (failedGuesses < guessesAllowed) {
   }
 
   //If you guessed correctly, but you still haven't guessed the whole word...
+
   else if (guessedCorrectly === true) {
     alert("You guessed correctly!");
   }
 
   //If you guess wrong:
+
   else if (guessedCorrectly === false) {
-    alert(
+    gameText(
       `You guessed wrong! You have ${guessesAllowed - failedGuesses} 
       guesses remaining!`
     );
   }
 
   displayString = displayArray.join(" ");
-  alert(displayString);
+  arrayText(displayString);
 }
+
+
+//*********************************//
+//*********************************//
+
+//FUNCTIONS:
+
+//*********************************//
+//*********************************//
+
+
 
 //This is the function for comparing two letters. It turns both to lower
 //case and compares them, and if they are the same, the function returns true.
 
 function compareLetters(letterA, letterB) {
   return letterA.toLowerCase() === letterB.toLowerCase();
+}
+
+//Function for displaying the text in the display array:
+
+function arrayText(arrayTextVar) {
+  let arrayTextEl = document.querySelector(".arrayTextClass");
+  arrayTextEl.innerHTML = arrayTextVar;
+}
+
+//Function for displaying game texts and prompts:
+
+function gameText(gameTextVar) {
+  let gameTextEl = document.querySelector(".gameTextClass");
+  gameTextEl.innerHTML = gameTextVar;
+}
+
+//Function for displaying a prompt, and parsing the form input to the relevant variable:
+
+function playerPrompt(promptMessage) {
+  let displayGameEl = document.querySelector(".gameTextClass");
+  displayGameEl.innerHTML = promptMessage;
+
+  let formEl = document.querySelector("#guessForm");
+  let guessValue;
+
+  formEl.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let guessEl = document.querySelector("#guess");
+    guessValue = guessEl.value;
+  });
+
+  return guessValue;
 }
